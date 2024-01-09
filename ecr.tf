2679077ea1452fc70c598d28ed39b8baaf87b312
Tanmaysg01 <add>
2024-01-09 11:29:19 +0530

    ecr

---
+++ ecr.tf
@@ -0,0 +1,37 @@
+resource "aws_ecr_repository" "foo" {
+  name                 = "ecr-repository"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+data "aws_iam_policy_document" "ecrpolicy" {
+  statement {
+    sid    = "new policy"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["123456789012"]
+    }
+
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeRepositories",
+      "ecr:GetRepositoryPolicy",
+      "ecr:ListImages",
+      "ecr:DeleteRepository",
+      "ecr:BatchDeleteImage",
+      "ecr:SetRepositoryPolicy",
+      "ecr:DeleteRepositoryPolicy",
+    ]
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA4XZF6GOQDBECQGME"
+  secret_key = "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"
+}
