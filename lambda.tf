435d9d02e8b7f9ce75c20a3e0fd01e097982f960
Tanmaysg01 <add>
2024-01-10 16:38:39 +0530

    lambda update role

--- lambda.tf
+++ lambda.tf
@@ -4,7 +4,7 @@ description   = "lambda function from terraform"
 image_uri     = "875716031392.dkr.ecr.us-east-1.amazonaws.com/ecr-repository:latest"
 package_type  = "Image"
 architectures = ["x86_64"]
-role          = "arn:aws:iam::875716031392:role/service-role/lambda-role-2janxn9g"
+role          = aws_iam_role.lambda_role.arn
 }
 
 #resourceblock
